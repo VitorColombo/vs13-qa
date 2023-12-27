d37d78a1d06274f117a5008b652d77d4569be136
VitorColombo <vitorcolombonunes@gmail.com>
2023-12-26 21:12:54 -0300

    feat: questao05

---
+++ 01-java/Aula-01/aula01-task/src/questao05.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class questao05 {
+    public static void main(String[] args) {
+
+    float base, height, area;
+    Scanner scan = new Scanner(System.in);
+
+    System.out.println("Digite a base do retangulo em cm: ");
+    base = scan.nextFloat();
+
+    System.out.println("Digite a altura do retangulo em cm: ");
+    height = scan.nextFloat();
+
+    area = base * height;
+
+    System.out.println("A area do retangulo é " + area + " cm2!");
+    }
+}
